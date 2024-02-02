fb571171e01b89ce53aae273e68309bc958e6ddd
shamnathasni <shamnathasni4@gmail.com>
2024-02-02 16:21:48 +0530

    Updated cors

--- App.js
+++ App.js
@@ -12,13 +12,13 @@ const PORT = process.env.PORT || 3000;
 const dbConnection = require("./Config/Configurationdb");
 
 dbConnection();
-App.use(CORS({
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
-}))
 // App.use(CORS({
-//   origin:"https://snapshot-studios.vercel.app",  
-//   credentials:true
+//   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
 // }))
+App.use(CORS({
+  // origin:"https://snapshot-studios.vercel.app",  
+  credentials:true
+}))
 // App.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*")
 //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
